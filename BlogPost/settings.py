d587ea0e1ecc9f424905d7d06e69a7c79d53bd0f
Abbas Abro <abroabbas44@gmail.com>
2025-06-13 16:02:12 +0500

    Update settings.py

--- BlogPost/settings.py
+++ BlogPost/settings.py
@@ -136,5 +136,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ="abroabbas44@gmail.com"
-EMAIL_HOST_PASSWORD = "xgkb oqze clzh rsvb"
+EMAIL_HOST_USER =""#Add Your email
+EMAIL_HOST_PASSWORD = "" #add the app password genertated from the gmail
